e93450def50383a6f968275d2ca0ff06caaf1fb3
rvpandey99 <rvpandey99@gmail.com>
2018-04-25 11:39:34 +0530

    Just created the file

---
+++ 1.2.3.FridayTheThirteen.cpp
@@ -0,0 +1,24 @@
+/*
+ID: rvpande1
+TASK: friday
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+	ofstream fout ("friday.out");
+    ifstream fin ("friday.in");
+
+	//system("pause");
+	return 0;
+}
+
+
